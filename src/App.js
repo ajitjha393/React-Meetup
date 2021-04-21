63d9012b677f1194593bcec7a349cbcc083e79a5
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-21 19:45:50 +0530

    Added Routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from 'react-router-dom'
+
+import AllMeetupsPage from './pages/AllMeetups/AllMeetups'
+import FavoritesPage from './pages/Favorites/Favorites'
+import NewMeetupPage from './pages/NewMeetup/NewMeetup'
+
 function App() {
-	return <div></div>
+	return (
+		<div>
+			<Route path="/">
+				<AllMeetupsPage />
+			</Route>
+			<Route path="/new-meetup">
+				<NewMeetupPage />
+			</Route>
+			<Route path="/favorites">
+				<FavoritesPage />
+			</Route>
+		</div>
+	)
 }
 
 export default App
