b8a6fcfb1e8cedc8d20e6d26e7c1316c33b758c6
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-22 15:44:16 +0530

    Passing Dummy data to render

--- src/pages/AllMeetups/AllMeetups.js
+++ src/pages/AllMeetups/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from '../../components/Meetups/MeetupList/MeetupList'
+
 const DUMMY_DATA = [
 	{
 		id: 'm1',
@@ -23,11 +25,7 @@ function AllMeetupsPage() {
 	return (
 		<section>
 			<h1>All Meetups</h1>
-			<ul>
-				{DUMMY_DATA.map(meetup => (
-					<li key={new Date().toISOString() + meetup.id}>{meetup.title}</li>
-				))}
-			</ul>
+			<MeetupList meetups={DUMMY_DATA} />
 		</section>
 	)
 }
