dc3f3e1560cf34ea599ff29dad7fd19aad74635d
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-22 15:49:13 +0530

    Added Card Wrapper

--- src/components/Meetups/MeetupItem/MeetupItem.js
+++ src/components/Meetups/MeetupItem/MeetupItem.js
@@ -1,20 +1,24 @@
+import Card from '../../UI/Card/Card'
+
 import classes from './MeetupItem.module.css'
 
 function MeetupItem({ title, address, description, image }) {
 	return (
 		<li className={classes.item}>
-			<div className={classes.image}>
-				<img src={image} alt={title} />
-			</div>
-			<div className={classes.content}>
-				<h3>{title}</h3>
-				<address>{address}</address>
-				<p>{description}</p>
-			</div>
+			<Card>
+				<div className={classes.image}>
+					<img src={image} alt={title} />
+				</div>
+				<div className={classes.content}>
+					<h3>{title}</h3>
+					<address>{address}</address>
+					<p>{description}</p>
+				</div>
 
-			<div className={classes.actions}>
-				<button>To Favorites</button>
-			</div>
+				<div className={classes.actions}>
+					<button>To Favorites</button>
+				</div>
+			</Card>
 		</li>
 	)
 }
