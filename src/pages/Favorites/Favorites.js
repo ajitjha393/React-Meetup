58d99f3770fad4a02dcd4765e899cf0f2dc97b0a
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 12:01:52 +0530

    Used favorites context in Favorites page

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,5 +1,16 @@
+import { useContext } from 'react'
+import FavoritesContext from '../../store/favorites-context'
+import MeetupList from '../../components/Meetups/MeetupList/MeetupList'
+
 function FavoritesPage() {
-	return <div>Favorites Page</div>
+	const { favorites } = useContext(FavoritesContext)
+
+	return (
+		<section>
+			<h1>Favorites Page</h1>
+			<MeetupList meetups={favorites} />
+		</section>
+	)
 }
 
 export default FavoritesPage
