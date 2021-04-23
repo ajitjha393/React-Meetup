f27736e38b121a1a024d2d62144827d4bedb6939
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-23 12:06:19 +0530

    Rendering default fallback msg in case of no favs

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -3,12 +3,19 @@ import FavoritesContext from '../../store/favorites-context'
 import MeetupList from '../../components/Meetups/MeetupList/MeetupList'
 
 function FavoritesPage() {
-	const { favorites } = useContext(FavoritesContext)
+	const { favorites, totalFavorites } = useContext(FavoritesContext)
+
+	let content = null
+	if (totalFavorites === 0) {
+		content = <p>You got no favorites yet. Start adding some?</p>
+	} else {
+		content = <MeetupList meetups={favorites} />
+	}
 
 	return (
 		<section>
 			<h1>Favorites Page</h1>
-			<MeetupList meetups={favorites} />
+			{content}
 		</section>
 	)
 }
